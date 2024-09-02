1c69df4038fbfca2b8c7cc35473819b7868145ad
DingDong039 <154868708+DingDong039@users.noreply.github.com>
2024-09-02 19:08:19 +0700

    add page home and book

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Book } from "./pages/Book";
+
+const Router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/book", element: <Book /> },
+]);
+
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={Router}></RouterProvider>
+    </>
+  );
+}
+
+export default App;
